d93353f18f8b5014c33d7d0067bc650dc590d5aa
Maria-the2nd <vanquish.ideas@gmail.com>
2025-04-17 12:18:48 +0100

    refactor: remove unused useEffect import from work-order-sheet component

--- app/(dashboard)/designer-flow/components/work-order-sheet.tsx
+++ app/(dashboard)/designer-flow/components/work-order-sheet.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useFieldArray, useForm } from 'react-hook-form';
 import * as z from 'zod';
